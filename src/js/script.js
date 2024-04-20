5c69cdd4a4c730677330c45044cfe620de3a3b4a
tomonaga osanai <to101850@gmail.com>
2024-04-20 16:00:25 +0900

    2回目のコミット

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,14 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+    $('.js-hamburger').on('click', function () {
+        if ($('.js-hamburger').hasClass('is-open')) {
+          $('.js-drawer-menu').fadeOut();
+          $(this).removeClass('is-open');
+        } else {
+          $('.js-drawer-menu').fadeIn();
+          $(this).addClass('is-open');
+        }
+      });
 
 
 });
