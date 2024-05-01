7f8c9cd8509ca622cc1f25228b29e4586a044e8f
tomonaga osanai <to101850@gmail.com>
2024-05-02 06:30:57 +0900

    3回目のコミット

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,35 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
     $('.js-hamburger').on('click', function () {
-        if ($('.js-hamburger').hasClass('is-open')) {
-          $('.js-drawer-menu').fadeOut();
-          $(this).removeClass('is-open');
+        if ($('.js-hamburger').hasClass('is-active')) {
+          // $('.js-drawer-menu').fadeOut();
+          $('.js-hamburger').removeClass('is-active');
+          $('.js-sp-nav').fadeOut(300);
+
         } else {
-          $('.js-drawer-menu').fadeIn();
-          $(this).addClass('is-open');
+          $('.js-hamburger').addClass('is-active');
+          $('.js-sp-nav').fadeIn(300);
         }
       });
+});
 
-
+var swiper = new Swiper(".js-location-swiper", {
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  centeredSlides: true,
+  slidesPerView: 1,
+  spaceBetween: 15,
+  breakpoints: {
+    767: {
+      slidesPerView: 3,
+      spaceBetween: 15
+    }
+  },
+  pagination: {
+    el: ".js-location-pagination",
+    clickable: true,
+  },
+  loop: true,
 });
